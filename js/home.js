d63cfe01954f21ed88ba6775390b0dcab4279252
Shackile Ally Asuncion <shackileallyasuncion@Ally-3.local>
2023-04-19 14:36:48 +0800

    blur js

--- js/home.js
+++ js/home.js
@@ -4,9 +4,34 @@ const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
         if (scrollY >= 200) {
-                navbar.classList.add('bg');
+                navbar.classList.add('background');
         } else{
-                navbar.classList.remove('bg');  
+                navbar.classList.remove('background');  
         }
         // console.log('scroll');
 })
+
+// Image Collage Blur Effect
+
+const collageImage = [...document.querySelectorAll('.collage-img')];
+
+collageImage.map( (item, i) => {
+        item.addEventListener('mouseover', () => {
+                collageImage.map((image, index) => {
+                        if(index != i){
+                                image.style.filter = `blur(3px)`;
+                                item.style.zIndex = 2;
+                        }
+                })
+        })
+} )
+
+collageImage.map( (item, i) => {
+        item.addEventListener('mouseleave', () => {
+                collageImage.map((image, index) => {
+                        if(index != i){
+                                image.style = null;
+                        }
+                })
+        })
+} )
